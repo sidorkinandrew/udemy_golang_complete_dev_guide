f764c0719af258f3f8a95a3f715d5dae166886ce
Sidorkin.Andrew <sidorkin.andrew@gmail.com>
2024-04-27 10:56:07 +0000

    expected_last_card, expected_first_card

--- deck_test.go
+++ deck_test.go
@@ -5,7 +5,18 @@ import "testing"
 func TestNesDeck(tester *testing.T) {
 	cards := newDeck()
 
-	if len(cards) != 52 {
-		tester.Errorf("Expected deck length of 52, but got %v", len(cards))
+	expected_length := 52
+	if len(cards) != expected_length {
+		tester.Errorf("Expected deck length of %v, but got %v", expected_length, len(cards))
+	}
+
+	expected_first_card := "Ace of Spades"
+	if cards[0] != expected_first_card {
+		tester.Errorf("Expected first card of %v, but got %v", expected_first_card, cards[0])
+	}
+
+	expected_last_card := "King of Clubs"
+	if cards[len(cards)-1] != expected_last_card {
+		tester.Errorf("Expected last card of %v, but got %v", expected_last_card, cards[len(cards)-1])
 	}
 }
